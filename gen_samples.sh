5f0e87ad283df3db9a080d3b5784a9a769ee6238
Andrew Cheng <a-cheng@berkeley.edu>
2024-09-06 11:27:38 -0700

    upate gen_samples and delete some test_data

--- gen_samples.sh
+++ gen_samples.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=40:00:00
@@ -7,5 +7,38 @@
 #SBATCH --cpus-per-gpu=10
 #SBATCH --partition=gpu
 
-pip install -e .
-aria sample -m large -c large-abs-inst.safetensors -p tests/test_data/bach.mid tests/test_data/arabseque.mid -var 10 -trunc 0 -l 800 -temp 0.95 -form ABA 
+# Directory containing files
+DIR="tests/test_data/"
+# Number of iterations
+ITERATIONS=10000
+
+# Function to generate random float between two values
+random_float() {
+  echo "$(awk -v min=$1 -v max=$2 'BEGIN{srand(); print min+(rand()*(max-min))}')"
+}
+
+# Function to generate random integer between two values
+random_int() {
+  echo $(( ( RANDOM % ($2 - $1 + 1) ) + $1 ))
+}
+
+for ((i=1; i<=ITERATIONS; i++)); do
+  # Get two random files from the directory
+  file1=$(ls "$DIR" | shuf -n 1)
+  file2=$(ls "$DIR" | shuf -n 1)
+  
+  # Ensure the two files are different
+  while [ "$file1" == "$file2" ]; do
+    file2=$(ls "$DIR" | shuf -n 1)
+  done
+
+  # Generate random values for -var, -temp, and -l
+  #var=$(random_int 1 20)
+  var=5
+  temp=$(random_float 0.5 1.5)
+  l=$(random_int 400 1000)
+
+  # Run the command with the generated values
+  aria sample -m large -c large-abs-inst.safetensors -p "$DIR/$file1" "$DIR/$file2" -var "$var" -trunc 0 -l "$l" -temp "$temp" -form ABA
+  
+done
